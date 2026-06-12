2a1a8e76d7327cbc3d8e560950f1552872f48254
Emmett Brown <bemmett886@outlook.com>
2026-06-12 02:22:46 -0500

    Added installed and live sound pages, started filling in live sound page

---
+++ navbar.js
@@ -0,0 +1,26 @@
+class NavBar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <nav>
+                <div class="navbar">
+                    <a href="index.html" class="logo">
+                        <img src="logo.png">
+                    </a>
+                    <div class="navlayers">
+                        <div class="navbuttons">
+                            <a class="individualbuttons" href="live.html">Live Sound</a>
+                            <a class="individualbuttons" href="installed.html">Installed Sound</a>
+                            <span class="spacer"></span>
+                            <a class="individualbuttons" href="about.html">About Us</a>
+                            <a class="individualbuttons" href="contact.html">Contact</a>
+                            <a class="individualbuttons" href="policies.html">Policies</a>
+
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        `;
+    }
+}
+
+customElements.define('nav-bar', NavBar);
