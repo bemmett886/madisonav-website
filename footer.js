060851fae60185a680221b8b9e5e1dba3f082bff
Emmett Brown <bemmett886@outlook.com>
2026-06-18 22:53:29 -0500

    1.0.4 - moved footer to javascript and put it on all pages, messed with homepage formatting a bit, added a sitemap

---
+++ footer.js
@@ -0,0 +1,15 @@
+class FooterFill extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="wholefooter">
+                <p class="policytext">&copy; 2026 Madison Audio Visual - Madison, SD 57042 - All Rights Reserved - Website v1.0.4</p>
+                <span class="spacer"></span>
+                <a href="sitemap.html" class="policytext">Sitemap</a>
+                <a href="contact.html" class="policytext">Contact</a>
+                <a href="about.html" class="policytext" style="white-space: nowrap;">About Us</a>
+            </div>
+        `;
+    }
+}
+
+customElements.define('footer-fill', FooterFill);
